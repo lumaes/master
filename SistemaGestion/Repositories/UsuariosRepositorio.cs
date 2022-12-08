5fd8692bc6ff04069c34379ddcf7b787258e30a7
lumaes <118992918+lumaes@users.noreply.github.com>
2022-12-07 22:38:22 -0300

    Update UsuariosRepositorio.cs

--- SistemaGestion/Repositories/UsuariosRepositorio.cs
+++ SistemaGestion/Repositories/UsuariosRepositorio.cs
@@ -59,5 +59,119 @@ namespace SistemaGestion.Repositories
             }
             return lista;
         }
+        public bool eliminarUsuario(int id)
+        {
+
+            if (conexion == null)
+            {
+                throw new Exception("Conexion no establecida");
+            }
+            try
+            {
+                int filasAfectadas = 0;
+                using (SqlCommand cmd = new SqlCommand("Delete From Usuario WHERE Id = @id", conexion))
+                {
+                    conexion.Open();
+                    cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.Int) { Value = id });
+                    filasAfectadas = cmd.ExecuteNonQuery();
+                }
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return false;
+        }
+        public void CrearUsuario(Usuario usuario)
+        {
+
+            if (conexion == null)
+            {
+                throw new Exception("Conexion no establecida");
+            }
+            try
+            {
+
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Usuario(Nombre, Apellido, NombreUsuario, Contraseña, Mail) VALUES (@nombre, @apellido, @nombreUsuario ,@contrasenia ,@mail)", conexion))
+                {
+                    conexion.Open();
+                    cmd.Parameters.Add(new SqlParameter("nombre", SqlDbType.VarChar) { Value = usuario.Nombre });
+                    cmd.Parameters.Add(new SqlParameter("apellido", SqlDbType.VarChar) { Value = usuario.Apellido });
+                    cmd.Parameters.Add(new SqlParameter("nombreUsuario", SqlDbType.VarChar) { Value = usuario.NombreUsuario });
+                    cmd.Parameters.Add(new SqlParameter("contrasenia", SqlDbType.VarChar) { Value = usuario.Contrasenia });
+                    cmd.Parameters.Add(new SqlParameter("mail", SqlDbType.VarChar) { Value = usuario.Mail });
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            conexion.Close();
+        }
+        public void editarUsuario(Usuario usuario)
+        {
+
+            if (conexion == null)
+            {
+                throw new Exception("Conexion no establecida");
+            }
+            try
+            {
+
+                using (SqlCommand cmd = new SqlCommand("UPDATE Usuario SET" +
+                                                      "     Nombre = @nombre," +
+                                                      "     Apellido = @apellido," +
+                                                      "     NombreUsuario = @nombreUsuario," +
+                                                      "     Contraseña = @contraseña," +
+                                                      "     Mail = @mail" +
+                                                      "     WHERE Id = @id ", conexion))
+                {
+                    conexion.Open();
+                    cmd.Parameters.Add(new SqlParameter("id", SqlDbType.Int) { Value = usuario.Id });
+                    cmd.Parameters.Add(new SqlParameter("nombre", SqlDbType.VarChar) { Value = usuario.Nombre });
+                    cmd.Parameters.Add(new SqlParameter("apellido", SqlDbType.VarChar) { Value = usuario.Apellido });
+                    cmd.Parameters.Add(new SqlParameter("NombreUsuario", SqlDbType.VarChar) { Value = usuario.NombreUsuario });
+                    cmd.Parameters.Add(new SqlParameter("contraseña", SqlDbType.VarChar) { Value = usuario.Contrasenia });
+                    cmd.Parameters.Add(new SqlParameter("mail", SqlDbType.VarChar) { Value = usuario.Mail });
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            conexion.Close();
+        }
+        public bool Login(Usuario usuario)
+        {
+
+            if (conexion == null)
+            {
+                throw new Exception("Conexion no establecida");
+            }
+            try
+            {
+
+                using (SqlCommand cmd = new SqlCommand("Select * From Usuario where Nombreusuario = @NombreUsuario AND Contraseña = @contraseña", conexion))
+                {
+                    conexion.Open();
+                    cmd.Parameters.Add(new SqlParameter("NombreUsuario", SqlDbType.VarChar) { Value = usuario.NombreUsuario });
+                    cmd.Parameters.Add(new SqlParameter("contraseña", SqlDbType.VarChar) { Value = usuario.Contrasenia });
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            conexion.Close();
+            return false;
+        }
     }
 }
