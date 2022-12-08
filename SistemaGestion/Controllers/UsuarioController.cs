9be869eaa240c8ce734ee08993e8a3bc8649d9df
lumaes <118992918+lumaes@users.noreply.github.com>
2022-12-07 22:40:25 -0300

    Update UsuarioController.cs

--- SistemaGestion/Controllers/UsuarioController.cs
+++ SistemaGestion/Controllers/UsuarioController.cs
@@ -24,5 +24,53 @@ namespace SistemaGestion.Controllers
                 return Problem(ex.Message);
             }
         }
+        [HttpDelete]
+        public ActionResult Delete([FromBody] int id)
+        {
+            try
+            {
+                bool seElimino = repository.eliminarUsuario(id);
+                if (seElimino)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Usuario usuario)
+        {
+            try
+            {
+                repository.CrearUsuario(usuario);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        [HttpPut]
+        public ActionResult Put([FromBody] Usuario usuario)
+        {
+            try
+            {
+                repository.editarUsuario(usuario);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
     }
 }
