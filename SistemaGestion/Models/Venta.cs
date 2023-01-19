9744d132caa5d792d08e0841e12e1d607407b3d1
lumaes <118992918+lumaes@users.noreply.github.com>
2023-01-19 20:27:15 -0300

    Update Venta.cs

--- SistemaGestion/Models/Venta.cs
+++ SistemaGestion/Models/Venta.cs
@@ -2,16 +2,22 @@
 {
     public class Venta
     {
-        public int Id { get; set; }
+       public int Id { get; set; }
         public string Comentarios { get; set; }
 
         public int idUsuario { get; set; }
+        public List<ProductoVendido> productoVendidos { get; set; }
 
         public Venta()
         {
-            Id = 0;
-            Comentarios = "";
-            idUsuario = 0;
+
+        }
+        public Venta(int id, string comentarios, int idUsuario)
+        {
+            this.Id = id;
+            this.Comentarios = comentarios;
+            this.idUsuario = idUsuario;
+            this.productoVendidos = new List<ProductoVendido>();
         }
     }
 }
