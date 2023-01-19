c13cc0202d31ebda7647db14b690872d1f6d6bdc
lumaes <118992918+lumaes@users.noreply.github.com>
2023-01-19 20:21:21 -0300

    Update VentaController.cs

--- SistemaGestion/Controllers/VentaController.cs
+++ SistemaGestion/Controllers/VentaController.cs
@@ -6,7 +6,7 @@ using System.Net;
 namespace SistemaGestion.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class VentaController : Controller
     {
 
@@ -14,13 +14,13 @@ namespace SistemaGestion.Controllers
         private VentaRepositorio repository = new VentaRepositorio();
 
         
-        // GET: VentaController
+        // TRAEMOS LA LISTA DE VENTAS
         [HttpGet]
         public ActionResult Get()
         {
             try
             {
-                List<Venta> lista = repository.ListarVenta();
+                List<Venta> lista = repository.ListarVenta(null);
                 return Ok(lista);
             }
             catch(Exception ex)
@@ -29,7 +29,72 @@ namespace SistemaGestion.Controllers
             }
            
         }
+        // TRAE EL PRODUCTO POR ID
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            try
+            {
+                List<Venta> lista = repository.ListarVenta(id);
+                return Ok(lista);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
 
-       
+        }
+        //ELIMINA EL PRODUCTO POR ID
+        [HttpDelete]
+        public ActionResult Delete([FromBody] int id)
+        {
+            try
+            {
+                bool seElimino = repository.eliminarVenta(id);
+                if (seElimino)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+
+        }
+        // CREA UNA VENTA
+        [HttpPost]
+        public ActionResult Post([FromBody] Venta venta)
+        {
+            try
+            {
+                repository.CrearVenta(venta);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        // MODIFICAR VENTA
+        [HttpPut]
+        public ActionResult Put([FromBody] Venta venta)
+        {
+            try
+            {
+                repository.editarVenta(venta);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
     }
 }
