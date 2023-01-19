e83414db94a00cc224bc3c7b4e98ba4594eba5fc
lumaes <118992918+lumaes@users.noreply.github.com>
2023-01-19 20:28:10 -0300

    Update Usuario.cs

--- SistemaGestion/Models/Usuario.cs
+++ SistemaGestion/Models/Usuario.cs
@@ -2,21 +2,26 @@
 {
     public class Usuario
     {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string NombreUsuario { get; set; }
-        public string Contrasenia { get; set; }
-        public string Mail { get; set; }
+         public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? NombreUsuario { get; set; }
+        public string? Contrasenia { get; set; }
+        public string? Mail { get; set; }
 
         public Usuario()
         {
-            Id = 0;
-            Nombre = "";
-            Apellido = "";
-            NombreUsuario = "";
-            Contrasenia = "";
-            Mail = "";
+
+        }
+
+        public Usuario(int id, string nombre, string apellido, string nombreUsuario, string contrasenia, string mail)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.NombreUsuario = nombreUsuario;
+            this.Contrasenia = contrasenia;
+            this.Mail = mail;
         }
 
     }
