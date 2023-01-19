332d12318c05920ef41e4d6ddd13e681b21e81a1
lumaes <118992918+lumaes@users.noreply.github.com>
2023-01-19 20:25:27 -0300

    Update Producto.cs

--- SistemaGestion/Models/Producto.cs
+++ SistemaGestion/Models/Producto.cs
@@ -6,18 +6,25 @@ namespace SistemaGestion.Models
     {
         public int id { get; set; }
 
-        public string Descripcion { get; set; }
+        public string? Descripciones { get; set; }
         public double Costo { get; set; }
         public double PrecioVenta { get; set; }
         public int stock { get; set; }
+        public int IdUsuario { get; set; }
 
+        public Producto(string descripciones, double precioVenta)
+        {
+            this.Descripciones = descripciones;
+            this.PrecioVenta = precioVenta;
+        }
         public Producto()
         {
             id = 0;
-            Descripcion = "";
+            Descripciones = "";
             Costo = 0;
             PrecioVenta = 0;
             stock = 0;
+            IdUsuario = 0;
         }
     }
     
