e12f9058ea760870133472809899cc285988221d
lumaes <118992918+lumaes@users.noreply.github.com>
2023-01-19 20:29:36 -0300

    Update ProductoVendidoRepositorio.cs

--- SistemaGestion/Repositories/ProductoVendidoRepositorio.cs
+++ SistemaGestion/Repositories/ProductoVendidoRepositorio.cs
@@ -1,4 +1,5 @@
-﻿using SistemaGestion.Models;
+using SistemaGestion.Models;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace SistemaGestion.Repositories
@@ -48,6 +49,39 @@ namespace SistemaGestion.Repositories
                             }
                         }
                     }
+                    foreach (ProductoVendido productoVendido in lista)
+                    {
+                        productoVendido.Producto = ProductosRepositorio.getSoloProductoPorId(productoVendido.IdProducto, conexion);
+                    }
+                    return lista;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+            
+        }
+        public bool eliminarProductoVendido(int id)
+        {
+
+            if (conexion == null)
+            {
+                throw new Exception("Conexion no establecida");
+            }
+            try
+            {
+                int filasAfectadas = 0;
+                using (SqlCommand cmd = new SqlCommand("Delete From ProductoVendido WHERE Id = @id", conexion))
+                {
+                    conexion.Open();
+                    cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.Int) { Value = id });
+                    filasAfectadas = cmd.ExecuteNonQuery();
                 }
                 conexion.Close();
             }
@@ -55,7 +89,65 @@ namespace SistemaGestion.Repositories
             {
                 throw;
             }
-            return lista;
+            return false;
+        }
+        public void CrearProductoVendido(ProductoVendido productoVendido)
+        {
+
+            if (conexion == null)
+            {
+                throw new Exception("Conexion no establecida");
+            }
+            try
+            {
+
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO ProductoVendido(Stock, IdProducto, IdVenta) VALUES (@stock, @idProducto, @idVenta)", conexion))
+                {
+                    conexion.Open();
+                    cmd.Parameters.Add(new SqlParameter("stock", SqlDbType.Int) { Value = productoVendido.Stock });
+                    cmd.Parameters.Add(new SqlParameter("idProducto", SqlDbType.Int) { Value = productoVendido.IdProducto });
+                    cmd.Parameters.Add(new SqlParameter("idVenta", SqlDbType.Int) { Value = productoVendido.IdVenta });
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            conexion.Close();
+        }
+        public void editarProductoVendido(ProductoVendido productoVendido)
+        {
+
+            if (conexion == null)
+            {
+                throw new Exception("Conexion no establecida");
+            }
+            try
+            {
+
+                using (SqlCommand cmd = new SqlCommand("UPDATE ProductoVendido SET" +
+                                                      "     Stock = @stock," +
+                                                      "     IdProducto = @idProducto," +
+                                                      "     IdVenta = @idVenta" +
+                                                      "     WHERE Id = @id ", conexion))
+                {
+                    conexion.Open();
+                    cmd.Parameters.Add(new SqlParameter("id", SqlDbType.Int) { Value = productoVendido.Id });
+                    cmd.Parameters.Add(new SqlParameter("stock", SqlDbType.Int) { Value = productoVendido.Stock });
+                    cmd.Parameters.Add(new SqlParameter("idProducto", SqlDbType.Int) { Value = productoVendido.IdProducto });
+                    cmd.Parameters.Add(new SqlParameter("idVenta", SqlDbType.Int) { Value = productoVendido.IdVenta });
+
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            conexion.Close();
         }
     }
 }
